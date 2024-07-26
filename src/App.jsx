ea163e6a6b612940c07cc6d1451089190f2e3b29
Kleaver Onani <122821288+Onakleaver@users.noreply.github.com>
2024-07-26 18:48:04 +0000

    Updates info and setup homepage to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kleaver Joseph Onani",
+  title: "Imports Manager & Software Engineering Student",
+  email: "",
+  gitHub: "Onakleaver",
+  instagram: "",
+  linkedIn: "Kleaver Onani",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@OnaniKleaver",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
