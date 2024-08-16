2d035f6fd39015f3e0cb23db087c0f6e26b3d15f
Kleaver Onani <122821288+Onakleaver@users.noreply.github.com>
2024-08-16 13:02:44 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27092,7 +27092,7 @@ var _stylesCss = require("./styles.css");
     email: "",
     gitHub: "Onakleaver",
     instagram: "",
-    linkedIn: "https://www.linkedin.com/in/kleaver-onani",
+    linkedIn: "kleaveronani",
     medium: "",
     twitter: "@OnaniKleaver",
     youTube: ""
